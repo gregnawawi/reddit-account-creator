2aa67ab9bba1a96013b2b8691d7739f9577f4f19
thaiduongme <me@thaiduong.org>
2022-01-25 15:15:54 +0700

    fix: redundant connect to db

--- auto/banCheck.js
+++ auto/banCheck.js
@@ -4,17 +4,6 @@ const { Account } = require("../models/account");
 const mongoose = require("mongoose");
 const config = require("config");
 
-mongoose
-  .connect(config.get("mongoDBHost"))
-  .then(() =>
-    console.log(`Connected to MongoDB host: ${config.get("mongoDBHost")}`)
-  )
-  .catch(() =>
-    console.error(
-      `FATAL ERROR: Can't connect to MongoDB host: ${config.get("mongoDBHost")}`
-    )
-  );
-
 const startBanCheck = async function () {
   cluster.setupMaster({
     exec: __dirname + "/banCheckWorker.js",
